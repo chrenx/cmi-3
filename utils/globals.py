1b812b4c5ef93ff50a34a75669e41575797a2057
chrenx <chrenx@umich.edu>
2024-11-08 01:08:36 +0000

    update

---
+++ utils/globals.py
@@ -0,0 +1,24 @@
+FEATS_MAP = {
+    "biological_sex": {
+        "male": 1,
+        "female": 0,
+    },
+    "infancy_vac": {
+        "wp": 1, 
+        "ap": 0,
+    },
+    "ethnicity": {
+        "unknown": 0, 
+        "hispanic or latino": 1,
+        "not hispanic or latino": 2,
+    },
+    "race": {
+        "unknown or not reported": 0,
+        "american indian/alaska native": 1,
+        "native hawaiian or other pacific islander": 2,
+        "black or african american": 3,
+        "white": 4,
+        "asian": 5,
+        "more than one race": 6,
+    }
+}
